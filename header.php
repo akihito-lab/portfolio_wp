35eba5285119b429edd9556e65c3c88faebd7f08
akihito-lab <89515983+akihito-lab@users.noreply.github.com>
2021-10-02 15:28:49 +0900

    update header

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
   <!-- フォントファミリー -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Dancing+Script:wght@400;700&family=PT+Serif:wght@400;700&family=Ephesis&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=PT+Serif:wght@400;700&family=Ephesis&display=swap" rel="stylesheet">
   <!-- フォントアイコン -->
   <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet">
     <?php wp_head(); ?>
