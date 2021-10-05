25bdd9d2a8426bf5f03cb32ccab49afe57f56691
akihito-lab <89515983+akihito-lab@users.noreply.github.com>
2021-10-05 16:12:40 +0900

    add contact error-animation

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -121,4 +121,17 @@ $( '#move-page-top' ).click(function(){
 	}
 ) ;
 
+// お問い合わせ・エラー時フォームまでスクロール
+ 
+   if ( $('.error')[0] ) {
+ 
+     $('.mw_wp_form').addClass('mw_wp_form_error');
+ 
+     var errorEl = $('.mw_wp_form').eq(0);
+ 
+     var position = errorEl.parent().offset().top - 100;
+ 
+ $('body,html').delay(200).animate({scrollTop:position}, 600, 'swing');
+ 
+ }
 });
